d14d60db9b444d644896f5e7b5cb98ef8b2d03f2
zbruno <zach@pocketderm.com>
2015-07-16 21:54:26 -0700

    updating github page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var s = Snap('#svg');
 
 var svgCenterY = $('#svg').height()/2;
-var svgCenterX = $('#svg').width()/2;
+var svgCenterX = $('#svg').width()/4.5;
 
 var arr = [];
 
@@ -10,10 +10,11 @@ var clickedItemId;
 
 var numAnimations = 0;
 
-$('.back-btn').hide();
+$('.back-btn, .description-container').hide();
 
 $('.back-btn').click(function() {
     $(this).hide();
+    $('.description-container').hide();
     reset(clickedItemId, clickedItem);
 });
 
@@ -21,12 +22,12 @@ var _onHover = function(el) {
     el.hover(
         function() {
             this.attr({
-                fill: "red"
+                fill: "#23b6d5"
             });
         },
         function() {
             this.attr({
-                fill: "black"
+                fill: "#777"
             });
         }
     )
@@ -47,32 +48,26 @@ var startAnim = function(id, snapCir) {
     numAnimations = 0;
     clickedItem = snapCir;
     clickedItemId = id;
-    scatter(id, snapCir);
-}
-
-var reset = function(id, snapCir) {
-    clickedItem.animate({
-        transform: 's1,1'
-    }, 500, null, function() {
-        unscatter(id, snapCir);
-        clickedItem = null;
-    });
-}
-
-var scatter = function(circleId, snapCir) {
     for (var i = 0; i < arr.length; i++) {
-        if(i !== circleId) {
+        if(i !== id) {
             scurry(arr[i]);
         }
     }
 }
 
-var unscatter = function(circleId, snapCir) {
-    for (var i = 0; i < arr.length; i++) {
-        if(i !== circleId) {
-            unscurry(arr[i]);
+var reset = function(id, snapCir) {
+    $('.back-btn, .description-container').hide();
+    clickedItem.animate({
+        transform: 's1,1',
+        fill: "#777"
+    }, 500, null, function() {
+        for (var i = 0; i < arr.length; i++) {
+            if(i !== id) {
+                unscurry(arr[i]);
+            }
         }
-    }
+        clickedItem = null;
+    });
 }
 
 var scurry = function(snapCir) {
@@ -93,8 +88,6 @@ var unscurry = function(snapCir) {
             transform: 'matrix(1,0,0,1,0,0)',
         }, 750, mina.backout);
     }, delay);
-
-    $('.back-btn').hide();
 }
 
 var expandThis = function() {
@@ -106,63 +99,55 @@ var expandThis = function() {
         var centerY = bBox.cy;
         var transY = svgCenterY - centerY;
         var transX = svgCenterX - centerX;
-        var _transform = 's5,5T' + transX + ',' + transY;
-
-        $('.back-btn').show();
+        var _transform = 's7,7T' + transX + ',' + transY;
 
         clickedItem.animate({
-            transform: _transform
-        }, 500);
+            transform: _transform,
+            fill: "#23b6d5"
+        }, 500, null, function() {
+            $('.back-btn').show();
+            var thisOne = clickedItem.node.id;
+
+            $.getJSON( "data.json", function( data ) {
+                var fillTitle = data.descriptions[thisOne].title;
+                var fillText = data.descriptions[thisOne].description;
+                $('.description-container').find('h3').html(fillTitle);
+                $('.description-container').find('p').html(fillText);
+                $('.description-container').show();
+            });
+        });
     }
 }
 
 var counter = 0;
 var createCircle = function(x, y, circum) {
     t = s.circle(x, y, circum);
-    t.attr({id: counter, class: 'circles'});
+    t.attr({id: counter, class: 'circles', fill: '#777'});
     clickHandler(counter, t);
     _onHover(t);
     counter++;
     arr.push(t);
 }
- 
-for (var i = 0; i < 6; i++) {
-    
-    if (i == 0) {
-        for (var z = 1; z < 6; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 25, 25);    
-        }
+
+var createLogo = function(startX, startY, radius) {
+    var radius = radius;
+    var diam = radius*2;
+    var startX = startX;
+    var startY = startY;
+
+    var lgCirPosX = [startX, startX, startX + diam + (diam*.22), startX + diam + (diam*.22), startX + diam + (diam*2.22), startX + diam + (diam*2.22), startX + diam + (diam*4.55), startX + diam + (diam*4.55), startX + diam + (diam*6.66), startX + diam + (diam*6.66)];
+    var lgCirPosY = [startY + diam + (diam*2.22), startY + diam + (diam*4.55), startY + diam + (diam*.22), startY + diam + (diam*6.66), startY, startY + diam + (diam*7.77), startY, startY + diam + (diam*7.77), startY + diam + (diam*.22), startY + diam + (diam*6.66)];
+
+    var smCirPosX = [startX + diam + (diam*.77), startX + diam + (diam*1.55), startX + diam + (diam*1.55), startX + diam + (diam*3.44), startX + diam + (diam*3.44), startX + diam + (diam*5.33), startX + diam + (diam*5.33)];
+    var smCirPosY = [startY + diam + (diam*3.44), startY + diam + (diam*1.55), startY + diam + (diam*5.33), startY + diam + (diam*.77), startY + diam + (diam*6.11), startY + diam + (diam*1.55), startY + diam + (diam*5.33)];
+
+    for (var i = 0; i < 10; i++) {
+        createCircle(lgCirPosX[i], lgCirPosY[i], radius);
     }
-    if (i == 1) {
-        for (var z = 1; z < 6; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 100, 25);    
-        }
-    }
-    if (i == 2) {
-        for (var z = 1; z < 3; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 175, 25);    
-        }
-    }
-    if (i == 3) {
-        for (var z = 1; z < 3; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 250, 25);    
-        }
-    }
-    if (i == 4) {
-        for (var z = 1; z < 6; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 325, 25);    
-        }
-    }
-    if (i == 5) {
-        for (var z = 1; z < 6; z++) {
-            var dist = 65 * z;        
-            createCircle(dist, 400, 25);    
-        }
+    for (var i = 0; i < 7; i++) {
+        createCircle(smCirPosX[i], smCirPosY[i], radius);
     }
 
 }
+
+createLogo(100, 0, 25);
